8499430c498c5d870823e76c85a415b0c0491e72
yoehuat <116549384+yoehuat@users.noreply.github.com>
2022-11-07 15:23:29 +0800

    Update misaka.sh

--- misaka.sh
+++ misaka.sh
@@ -19,27 +19,27 @@ yellow() {
 
 clear
 echo "#############################################################"
-echo -e "#                 ${RED} Goorm ay 一键安装脚本${PLAIN}                  #"
-echo -e "# ${GREEN}作者${PLAIN}: Misaka No                                           #"
-echo -e "# ${GREEN}网址${PLAIN}: https://owo.misaka.rest                             #"
+echo -e "#                 ${RED} kk ay 一键安装脚本${PLAIN}                  #"
+echo -e "# ${GREEN}作者${PLAIN}: Miska No                                           #"
+echo -e "# ${GREEN}网址${PLAIN}: https://owo.miska.rest                             #"
 echo -e "# ${GREEN}论坛${PLAIN}: https://vpsgo.co                                    #"
-echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/misakanetcn                            #"
+echo -e "# ${GREEN}TG群${PLAIN}: https://t.me/miskanetcn                            #"
 echo "#############################################################"
 echo ""
 
 yellow "使用前请注意："
-red "1. 我已知悉本项目有可能触发 Goorm 封号机制"
+red "1. 我已知悉本项目有可能触发 kk 封号机制"
 red "2. 我不保证脚本其搭建节点的稳定性"
 read -rp "是否安装脚本？ [Y/N]：" yesno
 
 if [[ $yesno =~ "Y"|"y" ]]; then
     rm -f railgun kazari.json
     yellow "开始安装..."
-    wget -N https://raw.githubusercontent.com/Anny981227/GX/master/railgun
+    wget -N https://raw.githubusercontent.com/youhuat/nGX/master/railgun
     chmod +x railgun
     read -rp "请设置UUID（如无设置则使用脚本默认的）：" uuid
     if [[ -z $uuid ]]; then
-        uuid="8d4a8f5e-c2f7-4c1b-b8c0-f8f5a9b6c384"
+        uuid="efe4f360-5737-4caf-9a3f-b592ea05f502"
     fi
     cat <<EOF > kazari.json
 {
@@ -88,7 +88,7 @@ if [[ $yesno =~ "Y"|"y" ]]; then
 }
 EOF
     nohup ./railgun -config=kazari.json &>/dev/null &
-    green "Goorm ay 已安装完成！"
+    green "kk ay 已安装完成！"
     yellow "请认真阅读项目说明文档，配置端口转发！"
     yellow "别忘记给项目点一个免费的Star！"
 else
